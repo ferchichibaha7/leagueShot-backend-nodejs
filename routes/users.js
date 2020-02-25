0556d9090fb76621d575ebc9c643abab41a0776e
Baha <ferchichibaha7@gmail.com>
2020-02-25 09:21:23 +0100

    basic back leagueshot

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,7 @@ const fetch = require('node-fetch');
 
 
 var url ='https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/';
+var url2 ='https://euw1.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/';
 var headers = {
     "X-Riot-Token": "RGAPI-e270dd5b-e3f6-4f3a-a9ff-cb7898e58b2c",
 }
@@ -15,6 +16,15 @@ var headers = {
 router.get('/:summoner',async (req, res, next)=> {
   var summoner = req.params.summoner;
  const fetchRes= await fetch(url+summoner, { method: 'GET', headers: headers});
+ const jsonRes= await fetchRes.json();
+ console.log(jsonRes)
+ res.json(jsonRes)
+});
+
+
+router.get('/game/:id',async (req, res, next)=> {
+  var id = req.params.id;
+ const fetchRes= await fetch(url2+id, { method: 'GET', headers: headers});
  const jsonRes= await fetchRes.json();
  res.json(jsonRes)
 });
