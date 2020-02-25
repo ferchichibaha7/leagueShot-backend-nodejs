7fccb4776c993f915765c54a0b638cf7d43f503a
Baha <ferchichibaha7@gmail.com>
2020-02-25 03:42:15 +0100

    connect to riot api and react

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const fetch = require('node-fetch');
+
+
+
+var url ='https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/';
+var headers = {
+    "X-Riot-Token": "RGAPI-e270dd5b-e3f6-4f3a-a9ff-cb7898e58b2c",
+}
+
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Api works fine!');
+router.get('/:summoner',async (req, res, next)=> {
+  var summoner = req.params.summoner;
+ const fetchRes= await fetch(url+summoner, { method: 'GET', headers: headers});
+ const jsonRes= await fetchRes.json();
+ res.json(jsonRes)
 });
 
 
